eec666cd5ebe11dc859cf9c89e68915d0890cd29
MoonPillGyeom <ansvkf0820@naver.com>
2025-03-28 17:19:15 +0900

    feat: setTimeout

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-function nodePractice() {
-  console.log('node 교과서 책 연습장')
+function Start() {
+    console.log('Start');
+    Work();
+    End();
+}
+
+function Work() {
+  setTimeout(() => {
+    console.log('Work');
+  }, 1000);
+}
+
+function End() {
+    console.log('End');
 }
