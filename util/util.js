3df177bf94455326d3c5f8326e3bedb36db6721b
MoonPillGyeom <ansvkf0820@naver.com>
2025-04-01 20:48:12 +0900

    3.5절 끝 util모듈

---
+++ util/util.js
@@ -0,0 +1,16 @@
+const util = require('util');
+const crypto = require('crypto');
+
+// util.deprecate: 함수가 deprecated 처리되었음을 알림
+const dontUseMe = util.deprecate((x, y) => {
+  console.log(x + y); 
+}, 'dontUseMe 함수는 deprecated되었으니 더 이상 사용하지 마세요!');
+dontUseMe(1, 2);
+
+// util.promisify: 콜백 패턴을 프로미스 패턴으로 변경
+const randomBytesPromise = util.promisify(crypto.randomBytes);
+randomBytesPromise(64).then((buf) => {
+  console.log(buf.toString('base64'));
+}).catch((error) => {
+  console.error(error);
+})
